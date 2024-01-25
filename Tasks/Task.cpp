390c908b7888576416d77f536da16f3d6d069b69
saim-x <x_aci@hotmail.com>
2024-01-25 22:27:48 +0500

    asd

--- Tasks/Task.cpp
+++ Tasks/Task.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 int main() {
+	int array_size;
+	cout << "Enter the size of array: ";
+	cin >> array_size;
+	int* array = new int[array_size];
+	for (int i = 0; i < array_size; i++)
+	{
+		cout << "Enter your" << i + 1 << "number\n";
+		cin >> array[i];
+	}
 
 }
