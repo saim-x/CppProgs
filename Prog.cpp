462677589ae1fe73f0aa763d84344725c4995af8
saim-x <x_aci@hotmail.com>
2024-01-25 21:21:34 +0500

    df

--- Prog.cpp
+++ Prog.cpp
@@ -2,12 +2,34 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 int main()
 {
     int number;
     cout << "Enter any number: \n";
     cin >> number;
+    string numberinstring = to_string(number);
+    int numberlength = numberinstring.length();
+    int* numberarray = new int[numberlength];
+
+
+    for (int i = 0; i < numberlength; i++) {
+        // Conversion of each character back to integer
+        numberarray[i] = numberinstring[i] - '0';
+    }
+
+    cout << "Individual digits of the number: ";
+    for (int i = 0; i < numberlength; i++) {
+        cout << numberarray[i] << " ";
+    }
+
+    delete[] numberarray;
+    return 0;
+
+
 
 
     
