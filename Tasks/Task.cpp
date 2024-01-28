0b857749699611ca79c265dfc1976e60ac046adf
saim-x <x_aci@hotmail.com>
2024-01-28 16:17:39 +0500

    l

--- Tasks/Task.cpp
+++ Tasks/Task.cpp
@@ -8,19 +8,33 @@ void b_main() {
 	int temp;
 	cout << "Enter the size of the array: ";
 	cin >> array_size;
-	int *array = new int[array_size];
-	for (int i = 0; i < array_size; i++)
+	if (array_size < 2)
 	{
-		cout << "Enter the " << i + 1 << " number ";
-		cin >> array[i];
+		cout << "Enter 2 or more numbers! ";
+		return ;
 	}
-	temp = 0;
-	int highest = array[0];
+	else {
+		int* array = new int[array_size];
+		for (int i = 0; i < array_size; i++)
+		{
+			cout << "Enter the " << i + 1 << " number ";
+			cin >> array[i];
+		}
+		temp = 0;
+		int highest = array[0];
+		for (int i = 0; i < array_size; i++)
+		{
+			if (temp > array[i]) {
 
+			}
+		}
+
+
+
+
+		delete[] array;
+	}
 	
-
-
-	delete[] array;
 	
 }
 int main() {
