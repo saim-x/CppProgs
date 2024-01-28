326cb00149f55cd7126c43f4c7d633778770a342
saim-x <x_aci@hotmail.com>
2024-01-28 16:11:03 +0500

    gfh

--- Tasks/Task.cpp
+++ Tasks/Task.cpp
@@ -2,15 +2,28 @@
 #include <string>
 #include <vector>
 using namespace std;
-int main() {
+
+void b_main() {
 	int array_size;
-	cout << "Enter the size of array: ";
+	int temp;
+	cout << "Enter the size of the array: ";
 	cin >> array_size;
-	int* array = new int[array_size];
+	int *array = new int[array_size];
 	for (int i = 0; i < array_size; i++)
 	{
-		cout << "Enter your" << i + 1 << "number\n";
+		cout << "Enter the " << i + 1 << " number ";
 		cin >> array[i];
 	}
+	temp = 0;
+	int highest = array[0];
 
+	
+
+
+	delete[] array;
+	
+}
+int main() {
+	b_main();
+	return 0;
 }
