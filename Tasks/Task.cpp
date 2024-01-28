3cbca7a85c66443c88102549656e8c6b59d17074
saim-x <x_aci@hotmail.com>
2024-01-28 16:25:15 +0500

    asd

--- Tasks/Task.cpp
+++ Tasks/Task.cpp
@@ -20,18 +20,17 @@ void b_main() {
 			cout << "Enter the " << i + 1 << " number ";
 			cin >> array[i];
 		}
-		temp = 0;
-		int highest = array[0];
-		for (int i = 0; i < array_size; i++)
+		int temp;
+		for (int i = 0; i < array_size-1; i++)
 		{
-			if (temp > array[i]) {
-
+			if (array[i] > array[i+1]) {
+				temp = array[i];
+				array[i] = array[i+1];
+				array[i+1] = temp;
 			}
 		}
-
-
-
-
+		//The Second Largest Number
+		cout << "The Second Largest Number is " << array[array_size - 2] << endl;
 		delete[] array;
 	}
 	
